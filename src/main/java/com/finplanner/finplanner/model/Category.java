67c5a4a69575332ce138002ad571556bae5ec17a
Daniel <57846982+Sayakp@users.noreply.github.com>
2025-06-10 23:01:59 -0500

    db, category, expense, and user initial setup

---
+++ src/main/java/com/finplanner/finplanner/model/Category.java
@@ -0,0 +1,35 @@
+package com.finplanner.finplanner.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity()
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    @ToString.Include
+    private String name;
+
+    @Column()
+    @ToString.Include
+    private String description;
+
+    // Categories without user will be considered global.
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
